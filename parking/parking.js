972fd2b7bb741060f40b88ba1ddc4434940a45cd
egpcommits <srtae1999@gmail.com>
2024-10-07 21:44:28 +0200

    parking al 80% completado

--- parking/parking.js
+++ parking/parking.js
@@ -6,31 +6,112 @@ let plazas = [[],[]];
 
 //Funciones que irán en un switch en el main.
 function aparcarVehiculo (tamanio) {
+    let coche = "";
+    do {
+        coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
+    } while ((coche != 'p') && (coche != 'g'));
 
+
+    let comprobarPequenias = plazas[0].some(function(elemento) {
+        return elemento == 'L';
+    });
+
+    let comprobarGrandes = plazas[1].some(function(elemento) {
+        return elemento == 'L';
+    });
+
+
+    if ((coche == 'p') && (comprobarPequenias)) {
+        let posicionPequenio = plazas[0].indexOf('L');
+        plazas[0][posicionPequenio] = 'O';
+        console.log(`Vehículo aparcado en P${posicionPequenio}`);
+    } else if ((coche == 'g') && (comprobarGrandes)) {
+        let posicionGrande = plazas[1].indexOf('L');
+        plazas[1][posicionGrande] = 'O';
+        console.log(`Vehículo aparcado en G${posicionGrande}`);
+    } else {
+        console.log("No se ha podido aparcar el vehículo.");
+    }
 }
 
 function sacarVehiculo (pos) {
+    let coche = "";
+    do {
+        coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
+    } while ((coche != 'p') && (coche != 'g'));
 
+    do {
+        pos = (prompt("Indique la plaza (de 0 a 13):"));
+    } while (((pos < 0) || (pos > 14)) || (isNaN(pos)));
+
+    if ((coche == 'p') && (plazas[0][pos] == 'O')) plazas[0][pos]  = 'L';        
+    else if ((coche == 'g') && (plazas[1][pos] == 'O')) plazas[1][pos]  = 'L';        
+    else console.log("En esa plaza no hay ningún coche aparcado..");
 }
 
 function mostrarAparcamiento () {
     console.log("Situación del aparcamiento: ");
+    
+    plazas.forEach((fila) => {
+        console.log(fila);
+    });
+    
+}
 
+function main () {
+    llenarAparcamiento();
+
+    let opcion = 0;
+    let salir = false;
+
+    while (!salir) {
+        do {
+            opcion = (Number(prompt(`Opción a elegir:
+                Aparcar vehículo - 1
+                Sacar vehículo - 2
+                Mostrar aparcamiento - 3
+                Salir - 4`)));
+        } while ((isNaN(opcion)) || (opcion < 1 || opcion > 4) );
+    
+        switch (opcion) {
+            case 1:
+                aparcarVehiculo();
+                break;
+            case 2:
+                sacarVehiculo();
+                break;
+            case 3:
+                mostrarAparcamiento();
+                break;
+            case 4:
+                salir = true;
+                console.log("Vuelva pronto a los aparcamientos de MOMO");
+        }
+    }    
+}
+
+
+
+
+
+
+//Funciones mias
+
+function llenarAparcamiento () {
     let i = 0, j = 0;
 
-    //Plzas pequeñas
+    //Plazas grandes
     while (i < 1) {
         j = 0;
         while (j < 10) {
             plazas[i].push('L');            
             j++;
         }
-        console.log(plazas[i]);
         i++;        
     }
 
 
-    //Plazas grandes
+    //Plazas pequeñas
     i = 1, j = 0;
 
     while (i < 2) {
@@ -39,38 +120,6 @@ function mostrarAparcamiento () {
             plazas[i].push('L');
             j++;
         }
-        console.log(plazas[i]);
         i++;
-    }
-}
-
-function salir () {
-
-}
-
-function main () {
-
-    let opcion = 0;
-
-    do {
-        opcion = (Number(prompt(`Opción a elegir:
-            Aparcar vehículo - 1
-            Sacar vehículo - 2
-            Mostrar aparcamiento -3
-            Salir - 4`)));
-    } while ((isNaN(opcion)) || (opcion < 1 || opcion > 4) );
-
-    switch (opcion) {
-        case 1:
-            aparcarVehiculo();
-            break;
-        case 2:
-            sacarVehiculo();
-            break;
-        case 3:
-            mostrarAparcamiento();
-            break;
-        case 4:
-            salir();
     }
 }
