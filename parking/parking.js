b0c15aeabe5273e76a9eeb0741143f28cf3d82ad
egpcommits <srtae1999@gmail.com>
2024-10-09 17:49:45 +0200

    Parking acabado al 100% - 9 de octubre

--- parking/parking.js
+++ parking/parking.js
@@ -6,47 +6,73 @@ let plazas = [[],[]];
 
 //Funciones que irán en un switch en el main.
 function aparcarVehiculo (tamanio) {
-    let coche = "";
+    let seguir = false; 
+    
     do {
-        coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
-    } while ((coche != 'p') && (coche != 'g'));
+        let coche = "";
+
+        do {
+            coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
+        } while ((coche != 'p') && (coche != 'g'));
 
 
-    let comprobarPequenias = plazas[0].some(function(elemento) {
-        return elemento == 'L';
-    });
+        let comprobarPequenias = plazas[0].some(function(elemento) {
+            return elemento == 'L';
+        });
 
-    let comprobarGrandes = plazas[1].some(function(elemento) {
-        return elemento == 'L';
-    });
+        let comprobarGrandes = plazas[1].some(function(elemento) {
+            return elemento == 'L';
+        });
 
 
-    if ((coche == 'p') && (comprobarPequenias)) {
-        let posicionPequenio = plazas[0].indexOf('L');
-        plazas[0][posicionPequenio] = 'O';
-        console.log(`Vehículo aparcado en P${posicionPequenio}`);
-    } else if ((coche == 'g') && (comprobarGrandes)) {
-        let posicionGrande = plazas[1].indexOf('L');
-        plazas[1][posicionGrande] = 'O';
-        console.log(`Vehículo aparcado en G${posicionGrande}`);
-    } else {
-        console.log("No se ha podido aparcar el vehículo.");
-    }
+        if ((coche == 'p') && (comprobarPequenias)) {
+            let posicionPequenio = plazas[0].indexOf('L');
+            plazas[0][posicionPequenio] = 'O';
+            console.log(`Vehículo pequeño aparcado en P${posicionPequenio}`);
+        } else if ((coche == 'g') && (comprobarGrandes)) {
+            let posicionGrande = plazas[1].indexOf('L');
+            plazas[1][posicionGrande] = 'O';
+            console.log(`Vehículo grande aparcado en G${posicionGrande}`);
+        } else if ((coche == 'p') && (!comprobarPequenias)) {
+            let posicionGrande = plazas[1].indexOf('L');
+            plazas[1][posicionGrande] = 'O';
+            console.log(`Vehículo pequeño aparcado en G${posicionGrande}`);
+        } else {
+            console.log("No se ha podido aparcar el vehículo.");
+        }
+
+        
+        seguir = (prompt("¿Aparcar otro? (S/N)").toLowerCase());
+        if (seguir === "s") seguir = true;
+        else seguir = false;
+
+    } while (seguir);
+
 }
 
 function sacarVehiculo (pos) {
-    let coche = "";
+    let seguir = false; 
+    
     do {
-        coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
-    } while ((coche != 'p') && (coche != 'g'));
 
-    do {
-        pos = (prompt("Indique la plaza (de 0 a 13):"));
-    } while (((pos < 0) || (pos > 14)) || (isNaN(pos)));
+        let coche = "";
+        do {
+            coche = (prompt("¿De qué tamaño es el coche? (P / G)").toLowerCase());
+        } while ((coche != 'p') && (coche != 'g'));
 
-    if ((coche == 'p') && (plazas[0][pos] == 'O')) plazas[0][pos]  = 'L';        
-    else if ((coche == 'g') && (plazas[1][pos] == 'O')) plazas[1][pos]  = 'L';        
-    else console.log("En esa plaza no hay ningún coche aparcado..");
+        do {
+            pos = (prompt("Indique la plaza (de 0 a 13):"));
+        } while (((pos < 0) || (pos > 14)) || (isNaN(pos)));
+
+        if ((coche == 'p') && (plazas[0][pos] == 'O')) plazas[0][pos]  = 'L';        
+        else if ((coche == 'g') && (plazas[1][pos] == 'O')) plazas[1][pos]  = 'L';        
+        else console.log("En esa plaza no hay ningún coche aparcado..");
+
+        seguir = (prompt("¿Sacar otro? (S/N)").toLowerCase());
+        if (seguir === "s") seguir = true;
+        else seguir = false;
+
+    } while (seguir);
 }
 
 function mostrarAparcamiento () {
@@ -85,7 +111,7 @@ function main () {
                 break;
             case 4:
                 salir = true;
-                console.log("Vuelva pronto a los aparcamientos de MOMO");
+                console.log("Vuelva pronto a nuestro aparcamiento!!");
         }
     }    
 }
