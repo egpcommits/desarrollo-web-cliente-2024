8769c7212cf94adca6da4cc12b2915ac5b03ea82
egpcommits <181844308+egpcommits@users.noreply.github.com>
2025-02-11 10:25:34 +0100

    Ya salen algunas imagenes - 11 de febrero

--- 08_asincronia/asincronia03/asincronia03.js
+++ 08_asincronia/asincronia03/asincronia03.js
@@ -25,6 +25,34 @@ document.addEventListener("DOMContentLoaded", () => {
             for(productos of datos) {
                 crearLista(productos["title"], productos["price"]);
             }
+
+            let longitud = document.getElementsByTagName("li").length;
+
+            for(let i = 0; i < longitud; i++) {
+                let lis = document.getElementsByTagName("li")[i];
+                let sectionImagen = document.getElementsByTagName("section")[1];
+                let imagen = document.createElement("img");
+                lis.addEventListener("mouseover", () => {
+                    let separado = lis.innerText.split(" - (");
+                    for(productos of datos) {
+                        if (productos["title"] == separado[0]) {     
+                            console.log(separado[0]);
+                            console.log(productos["title"]);
+                            
+                                                   
+                            imagen.setAttribute("src", productos["image"]);
+                            sectionImagen.appendChild(imagen);
+                            
+                        }
+                    } 
+                });
+                lis.addEventListener("mouseout", () => {
+                    imagen.setAttribute("src", "");
+                    sectionImagen.appendChild(imagen);
+                });
+            }
+            
+            
             
         })
         .catch((error) => {
